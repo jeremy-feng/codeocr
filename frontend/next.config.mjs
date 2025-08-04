7a8817e68f9c42264e712c3969eb1c8c129af410
jeremy-feng <jeremy-feng@qq.com>
2025-08-04 19:10:29 +0800

    chore: update Docker configuration to fix frontend deployment
    Remove the standalone output configuration from next.config.mjs, and adjust the Dockerfile to deploy the frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   compiler: {
     removeConsole:
       process.env.NODE_ENV === "production"
